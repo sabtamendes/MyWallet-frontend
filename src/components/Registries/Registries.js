c6f1322f8c34486b433cd70bb2c69eca9a274fae
Sabta Mendes <sabtaamendes@gmail.com>
2022-11-16 11:42:17 -0400

    reafact layout router registries

--- src/components/Registries/Registries.js
+++ src/components/Registries/Registries.js
@@ -1,18 +1,32 @@
 import styled from "styled-components";
 import Navbar from "../Navbar/Navbar";
-import Footer from "../Footer/Footer";
+import { IoAddCircleOutline } from "react-icons/io5";
+import { IoRemoveCircleOutline } from "react-icons/io5";
 export default function Registries() {
     return (
         <>
             <Navbar />
             <Container>
                 <ListTransactions>
-                    <span>Não há registros de 
+                    <span>Não há registros de
                         <br>
-                    </br> entrada ou saída</span>
+                        </br> entrada ou saída</span>
                 </ListTransactions>
+                
+                <Rodape>
+                    <EnterBox>
+                        <span><IoAddCircleOutline color="#ffffff" size="30px" /></span>
+
+                        <p>Nova entrada</p>
+                    </EnterBox>
+
+                    <OutBox>
+                        <IoRemoveCircleOutline color="#ffffff" size="30px" />
+                        <p>Nova<br></br>saída</p>
+                    </OutBox>
+                </Rodape>
+
             </Container>
-            <Footer />
         </>
     )
 }
@@ -44,5 +58,54 @@ span{
     display:flex;
     justify-content: center;
     align-items:center;
+}
+`
+const Rodape = styled.div`
+width:100%;
+padding-left: 28px;
+padding-right:28px;
+position:fixed;
+z-index: 2;
+bottom: 0;
+right:0;
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+`
+const EnterBox = styled.button`
+background-color: #A328D6;
+border-radius: 5px;
+border: none;
+margin-bottom: 15px;
+padding-top: 1vh;
+padding-bottom: 1vh;
+padding-right: 15vw;
+p{
+    font-size: 15px;
+    font-weight: bold;
+    font-family: Raleway;
+    color: #ffffff;
+    margin-top: 70px;
+}
+span{
+    margin-left: -65px;
+}
+`
+const OutBox = styled.button`
+background-color: #A328D6;
+border-radius: 5px;
+border: none;
+margin-bottom: 15px;
+padding-top: 1vh;
+padding-bottom: 1vh;
+padding-right: 30vw;
+padding-left: 5px;
+p{
+    font-size: 15px;
+    font-weight: bold;
+    font-family: Raleway;
+    color: #ffffff;
+    margin-top: 50px;
 }
 `
